c2fa3721bd86f2b5ea619c37942a7346cd754551
YandelC7R <cristoferverasanchez@gmail.com>
2026-07-17 15:02:32 -0500

    Cambio agregado en nueva rama

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
     echo "Hola, este es el taller 3 de Fundamentos de Software Libre - Cristoffer";
     echo "<br>Fecha: " . date("d/m/Y");
+    echo "<br>Actualizacion desde la nueva rama";
 ?>
